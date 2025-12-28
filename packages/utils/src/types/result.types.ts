942b2215630d2ede9799667e91d60dc3ad723c66
Igor Yuzkiv <igor97w@gmail.com>
2025-12-28 19:30:01 +0200

    -

--- packages/utils/src/types/result.types.ts
+++ packages/utils/src/types/result.types.ts
@@ -4,4 +4,6 @@ export type Error<E> = { ok: false; error: E }
 
 export type Result<T, E = string> = Ok<T> | Error<E>
 
+export type PromiseResult<T, E = string> = Promise<Result<T, E>>
+
 export type Maybe<T> = T | null | undefined
