806b89f961dc970f599b6f05ef30d4faf1ea09ac
Igor Yuzkiv <igor97@gmail.com>
2026-02-15 13:50:16 +0200

    feat: remove console.log statements from tab management functions

--- apps/chrome-ext/src/store/use.browser-tabs.store.ts
+++ apps/chrome-ext/src/store/use.browser-tabs.store.ts
@@ -22,8 +22,6 @@ export const useBrowserTabsStore = defineStore('browser.tabs', () => {
         const nextTabsMap = new Map(tabsMap.value)
         nextTabsMap.set(event.tab.id, event.tab)
         tabsMap.value = nextTabsMap
-
-        console.log(tabsList.value);
     }
 
     function handleTabRemoved(event: BrowserTabRemoveEvent) {
@@ -32,8 +30,6 @@ export const useBrowserTabsStore = defineStore('browser.tabs', () => {
         const nextTabsMap = new Map(tabsMap.value)
         nextTabsMap.delete(event.tabId)
         tabsMap.value = nextTabsMap
-
-        console.log(tabsList.value);
     }
 
     async function initialize() {
